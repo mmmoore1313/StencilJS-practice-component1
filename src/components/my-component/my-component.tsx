4252ee7ba36b9b5835cde48b78cd0ebfc73536dd
Matt Moore <Matt.M.Moore13@outlook.com>
2021-10-07 17:00:54 -0400

    removed the commented out hide button code

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,10 +1,9 @@
 import { Component, Prop, h, Method, State } from '@stencil/core';
-// import { format } from '../../utils/utils';
 
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true,
+  shadow: true
 })
 export class MyComponent {
   /**
@@ -18,6 +17,7 @@ export class MyComponent {
   @Prop() content: string;
   @State() openModalState: boolean = false;
 
+
   @Method()
   openModal() {
     this.openModalState = true;
@@ -29,27 +29,15 @@ export class MyComponent {
 
   render() {
     if (!this.openModalState) {
-      return (
-        <div>
-          {/*<button onClick={() => this.openModal()}>
-            Open
-          </button>*/}
-        </div>
-      )
+      return <div></div>;
     }
     return (
       <div class="backdrop-div">
 
         <div class="modal-div">
-          <button onClick={() => this.closeModal()}>
-            close
-          </button>
-          <h4 class="modal-header">
-             {this.title}
-          </h4>
-          <h4>
-            {this.content}
-          </h4>
+          <div onClick={() => this.closeModal()} class="close-button">close</div>
+          <h4 class="modal-header"> {this.title}</h4>
+          <h4>{this.content}</h4>
         </div>
       </div>
     );
